3d689e01f4019cd37074e9b88b214afd2b6683eb
ShunMitsuoka <s.mitsuoka@and-ex.co.jp>
2023-07-10 09:40:38 +0900

    vite修正

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     server: {
         host: true,
+        hmr: {
+            host: 'localhost',
+        }
     },
     plugins: [
         laravel({
